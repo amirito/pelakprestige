939ce76c4e97daf2ed7e1126a2d8f21bd70665a5
amirito <amirmousavi_90@yahoo.com>
2015-04-09 18:18:37 +0330

    first sample

---
+++ include/contact.php
@@ -0,0 +1,68 @@
+<section>
+	<h2>لطفا حتما با ما در تماس باشید!</h2>
+    <p>
+    	چطور می تونیم در خدمت شما باشیم؟  در پلاک پرستیژ ما از سوالات نظرات  پیشنهادات و تحسین های شما عزیزانی که ساکن در مجتمع های تحت مدیریت ما هستید خوش حال می شویم  و حتی انتقادات شما  عزیزان  نیز باعث شادمانی ماست چرا که با استفاده از آن ها می توانیم نواقص کار خودمان را بهتر بشناسیم و در خدمت رسانی به شما بیشتر تلاش کنیم. لطفا از روش های زیر با ما در ارتباط باشید
+    </p>
+    <p>
+    	 دفتر مرکزی: تهران- خ ظفر- کوچه  خالد- پلاک 3
+    </p>
+    <p>
+    	پشتیبانی ساکنین: 09122490804
+    </p>
+    <p>
+    	بازاریابی: 09122490804
+    </p>
+    <p>
+    	فکس: ........................
+    </p>
+    <p>
+    	info@pelakprestige.com   
+    </p>
+    <p>
+    	در صورت تمایل می توانید با تکمیل فرم زیر منتظر تماس ما باشید:
+    </p>
+    <form id="contact-form" role="form" method="post">
+    	<div class="form-group name">
+            <label for="name">نام ساختمان یا مجتمع شما (در صورت وجود )</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group name">
+            <label for="name">نام </label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group name">
+            <label for="name"> نام خانوادگی</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group message">
+            <label for="message">آدرس</label>
+            <textarea class="form-control" rows="6" id="content" name="message"  required></textarea>
+        </div>
+        <div class="form-group name">
+            <label for="name">شهر</label>
+            <select class="form-control" id="name" name="name" required>
+            	<option>تهران</option>
+                <option>شیراز</option>
+                <option>اصفهان</option>
+                <option>کاشان</option>
+                <option>یزد</option>
+                <option>کیش</option>
+                <option>مشهد</option>
+                <option>تبریز</option>
+            </select>
+        </div>
+        <div class="form-group name">
+            <label for="name">تلفن</label>
+            <input type="text" class="form-control" id="phone" name="phone" required>
+        </div>
+        <div class="form-group email">
+            <label for="email">ایمیل</label>
+            <input type="email" class="form-control" id="mail" name="mail"  required>
+        </div>        
+        <div class="form-group message">
+            <label for="message">سوال شما</label>
+            <textarea class="form-control" rows="6" id="content" name="message"  required></textarea>
+        </div>
+        <input name="submit" type="submit" value="ارسال پیام" class="btn btn-default">
+    </form>
+</section>
---
+++ main.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Rayweb</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+<body>
+<div class="col-md-6 left-side">
+	<?php
+		if(isset($_GET['page'])){
+					if(is_file('include/'.$_GET['page'].'.php')){
+						include 'include/'.$_GET['page'].'.php';
+						}else{
+						die('صفحه مورد نظر وجود ندارد');
+						}
+					}else{
+						header('Location: index.html');
+						}
+	
+	
+	?>
+</div>
+<div class="col-md-6 right-side">
+	<nav class="navbar navbar-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+    
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+          
+          <ul class="nav navbar-nav navbar-right">
+            <li class="pull-right"><a href="#">صفحه اصلی</a></li>
+            <li class="pull-right"><a href="#">درباره ما</a></li>
+            <li class="dropdown pull-right">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="caret"></span> ارتباط با ما </a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="?page=contact">تماس با ما </a></li>
+                <li><a href="#">دفاتر و پروژه های شرکت </a></li>
+                <li><a href="#">درخواست فرم  پیشنهاد همکاری</a></li>
+              </ul>
+            </li>
+            <li class="pull-right"><a href="#">فرصت های شغلی</a></li>
+            <li class="dropdown pull-right">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="caret"></span> اطلاعات ضروری ساکنین </a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
